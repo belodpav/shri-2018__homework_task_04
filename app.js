a393029d80434f4558e05176db23afb7a21d398b
belodpav <belod.pav@gmail.com>
2018-03-22 15:32:57 +0300

    Для следования ES6 синтаксису колбэков в app.js

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const router = require('./routes/index');
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.param(['id', 'co'], function(req, res, next, id) {
+app.param(['id', 'co'], (req, res, next, id) => {
     next();
 });
 
 app.use('/static', express.static('dist'));
 app.use(router);
 
-app.listen(PORT, HOST, function() {
+app.listen(PORT, HOST, () => {
     /* eslint-disable no-console */
     console.log(`Application is running on port ${PORT}!`);
     /* eslint-enable no-console */
