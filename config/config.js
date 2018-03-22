74e369421fe2eabca4afd9462b58eb9ec5081cf0
belodpav <belod.pav@gmail.com>
2018-03-23 02:22:12 +0300

    Для более удобного использования
    Измено имя конфига, так же изменена ссылка на тестируемы репозиторий

--- config/config.js
+++
@@ -1,3 +0,0 @@
-exports.PATH = '/home/pavel/dev/shri/exp/shri-2018__git_test';
-exports.PORT = '3000';
-exports.HOST = '0.0.0.0';
---
+++ config/index.js
@@ -0,0 +1,4 @@
+exports.PATH = '/home/pavel/dev/shri/exp/test-repo';
+exports.PORT = '3000';
+exports.HOST = '0.0.0.0';
+exports.DEFAULT_BRANCH = 'master';
