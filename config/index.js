9908c7fdb77e143e1f603677205ad34b91e3222a
belodpav <belod.pav@gmail.com>
2018-03-24 23:51:13 +0300

    heroku

--- config/index.js
+++ config/index.js
@@ -2,4 +2,3 @@ exports.PATH = process.env.REPO || '/home/pavel/dev/shri/exp/test-repo';
 exports.PORT = process.env.PORT || '3000';
 exports.HOST = '0.0.0.0';
 exports.DEFAULT_BRANCH = 'master';
-console.log(process.env.REPO);
