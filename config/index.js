b989dddc7690bcf3fa21302f04ca79587c875236
belodpav <belod.pav@gmail.com>
2018-03-24 19:08:33 +0300

    Для натройки докера

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-exports.PATH = '/home/pavel/dev/shri/exp/test-repo';
+exports.PATH = process.env.REPO || '/home/pavel/dev/shri/exp/test-repo';
 exports.PORT = '3000';
 exports.HOST = '0.0.0.0';
 exports.DEFAULT_BRANCH = 'master';
